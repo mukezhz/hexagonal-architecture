580ee8d1c03227f1979edef470ad8541057ff6ec
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2023-08-24 23:58:12 +0545

    feat: add port to save route store data in db

--- file/domain/port_excel.go
+++ file/domain/port_excel.go
@@ -3,8 +3,13 @@ package domain
 type ExcelIncomingPort interface {
 	Extract(filePath string) error
 	Print()
+	SaveToDB() error
 }
 
 type ExcelOutgoingPort interface {
 	GetAll(filePath string) ([]RouteStore, error)
+}
+
+type ExcelRepository interface {
+	CreateExcel(data []RouteStore) error
 }
