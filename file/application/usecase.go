c175c8d9584f2388283b317c476dc2a22424f51a
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2023-08-24 17:23:31 +0545

    feat: add ports and adapter of ports

--- file/application/usecase.go
+++ file/application/usecase.go
@@ -6,18 +6,24 @@ import (
 )
 
 type FileUseCase struct {
-	FileRepo domain.FileRepository
+	FileSystem domain.FileOutgoingPort
+	Repository domain.FileRepository
 }
 
-func NewFileUseCase(fileRepo domain.FileRepository) *FileUseCase {
-	return &FileUseCase{
-		FileRepo: fileRepo,
-	}
+func NewFileUseCase(fileRepo domain.FileOutgoingPort, repository domain.FileRepository) *FileUseCase {
+	return &FileUseCase{FileSystem: fileRepo, Repository: repository}
 }
 
 func (f *FileUseCase) Upload(file *multipart.FileHeader, dst string) error {
 	// business logic to check weather file should be save or not
-	if err := f.FileRepo.Save(file, dst); err != nil {
+	if err := f.FileSystem.Save(file, dst); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (f *FileUseCase) Save(file domain.FileMetadata) error {
+	if err := f.Repository.Create(file); err != nil {
 		return err
 	}
 	return nil
--- file/domain/ports.go
+++ file/domain/ports.go
@@ -2,10 +2,17 @@ package domain
 
 import "mime/multipart"
 
+/* ========== incoming ports ============= */
 type FilePort interface {
 	Upload(file *multipart.FileHeader, dst string) error
+	Save(file FileMetadata) error
+}
+
+/* ========== outgoing ports ============= */
+type FileOutgoingPort interface {
+	Save(file *multipart.FileHeader, dst string) error
 }
 
 type FileRepository interface {
-	Save(file *multipart.FileHeader, dst string) error
+	Create(file FileMetadata) error
 }
